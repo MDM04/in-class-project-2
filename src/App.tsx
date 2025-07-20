bcbf31bb355ecb517604024339865205cc10e743
MDM <mdm.pro4@gmail.com>
2025-07-20 19:30:08 -0300

    feat(routes) adicionando rotas, home hystory

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
-
-import {ThemeProvider} from "styled-components"
-import { Button } from "./components/Button";
+import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { Router } from "./components/Router";
+import { BrowserRouter } from "react-router-dom";
 
-export function App () {
+export function App() {
   return (
     <>
-    <ThemeProvider theme={defaultTheme}>
-      <Button  variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
-      <GlobalStyle/>
-    </ThemeProvider>
+      <ThemeProvider theme={defaultTheme}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+        <GlobalStyle />
+      </ThemeProvider>
     </>
-  )
+  );
 }
